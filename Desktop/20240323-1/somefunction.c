7e5e164c648f6f46d867ba2e4610fe05e5cdda7f
LZL <lizhuolin2023@hotmail.com>
2024-03-23 04:27:38 +0800

    test

---
+++ Desktop/20240323-1/somefunction.c
@@ -0,0 +1,12 @@
+// somefunction.cpp
+
+#include "somefunction.h"
+float myOtherVariable2 = 0;
+float myFunction(int myVariable,float *myOtherVariable) {
+    for(int i = 0; i < myVariable; i++) 
+    {
+        myOtherVariable2+=myOtherVariable[i];
+    }
+    return myOtherVariable2;
+    
+}
---
+++ Desktop/20240323-1/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "somefunction.h"
+#include "somefunction.c"
+#include <stdlib.h>
+int main() 
+{
+    float result ;
+    int size = 10;
+    scanf("%d", &size);
+    
+    float* a = (float*)malloc(size * sizeof(float)); // 动态分配一个浮点数数组
+    for (int i = 0; i < size; i++) {
+        scanf("%f",&a[i]); // 给数组赋值
+    }
+    result = myFunction(size, a); // 调用函数
+    printf("%.3f\n", result); // 输出结果
+    free(a); // 释放动态分配的数组
+    return 0;
+}
